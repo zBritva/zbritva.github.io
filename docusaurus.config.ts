308b3a060c280b253777e9184eb7fd8885711a7d
Ilfat Galiev <zbritva@gmail.com>
2023-11-24 15:41:21 +0400

    Add social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -61,7 +61,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/social-card.svg',
     navbar: {
       title: 'Ilfat Galiev',
       logo: {
