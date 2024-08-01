0546d357371322183e5985dc6b24974ef5b4fb04
Ilfat Galiev <zbritva@gmail.com>
2024-08-01 00:15:37 -0800

    Deploy website - based on 253db5f3e6a78a93dbc2808c12dc0b5d95ee40a9

---
+++ assets/js/56f8509e.a53bf9cb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkilfatgaliev=self.webpackChunkilfatgaliev||[]).push([[897],{6575:a=>{a.exports=JSON.parse('{"tag":{"label":"Mermaid","permalink":"/tags/mermaid","allTagsPath":"/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/tags/mermaid","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/57fcaf1b.1e3aac10.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkilfatgaliev=self.webpackChunkilfatgaliev||[]).push([[6164],{3985:a=>{a.exports=JSON.parse('{"tag":{"label":"markdown","permalink":"/tags/markdown","allTagsPath":"/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/tags/markdown","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
