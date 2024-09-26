c78d684fbdbc0a7d08521e29da3bfb6d4133abc1
Ilfat Galiev <zbritva@gmail.com>
2024-09-25 20:37:48 -0800

    Deploy website - based on 3045e6f085a9b286de6bb47e95d57bbeb0b8871c

---
+++ assets/js/1a1424c7.63ef3ebe.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkilfatgaliev=self.webpackChunkilfatgaliev||[]).push([[3370],{7996:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/","page":1,"postsPerPage":10,"totalPages":2,"totalCount":12,"nextPage":"/page/2","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
