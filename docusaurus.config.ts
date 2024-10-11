432113f855be35b15affc07f6131458e6784c368
Ilfat Galiev <zbritva@gmail.com>
2024-10-11 15:33:25 -0800

    Switch url from http to https

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'http://ilfat-galiev.im/',
+  url: 'https://ilfat-galiev.im/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
