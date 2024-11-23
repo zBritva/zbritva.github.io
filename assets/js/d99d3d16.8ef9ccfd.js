20cef8d049ef9105fdcfcc4291e25eb5fe75c934
Ilfat Galiev <zbritva@gmail.com>
2024-11-23 13:14:13 -0800

    Deploy website - based on 432113f855be35b15affc07f6131458e6784c368

---
+++ assets/js/d99d3d16.8ef9ccfd.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkilfatgaliev=self.webpackChunkilfatgaliev||[]).push([[1287],{4667:e=>{e.exports=JSON.parse('{"tag":{"label":"Power BI","permalink":"/docs/tags/power-bi","allTagsPath":"/docs/tags","count":1,"items":[{"id":"index","title":"Visual documentations","description":"Some visuals in development phase","permalink":"/docs/"}],"unlisted":false}}')}}]);
